2182f9c06953904d196560a12ef021ef0e3f77e4
Akshatha <110625776+Akshatha2@users.noreply.github.com>
2022-12-29 16:02:56 +0530

    Update deleteConso.java

--- deleteConso.java
+++ deleteConso.java
@@ -1,9 +1,8 @@
 import java.util.Scanner;
 class deleteConso
-
 {
-    public static void main(String args[])
-    {
+  public static void main(String args[])
+  {
     String s;
     int j=0;
     System.out.println("Enter a string");
@@ -14,20 +13,20 @@ class deleteConso
 
     for(int i=0;i<s.length();i++)
     {
-    if(s.charAt(i)=='a'|| s.charAt(i)=='e'|| s.charAt(i)=='i'||s.charAt(i)=='o'||  s.charAt(i)=='u' || s.charAt(i)==' ')
-    {
-     ch[j++]=s.charAt(i);
+      if(s.charAt(i)=='a'|| s.charAt(i)=='e'|| s.charAt(i)=='i'||s.charAt(i)=='o'||  s.charAt(i)=='u' || s.charAt(i)==' ')
+      {
+        ch[j++]=s.charAt(i);
+      }
+      else
+      {
+        continue;
+      }
     }
-    else
-    {
-     continue;
-    }
-    }
+      
     for(int i=0;i<j;i++)
     {
-    System.out.print(ch[i]);
+      System.out.print(ch[i]);
     }
     System.out.println();
-}
-
+  }
 }
